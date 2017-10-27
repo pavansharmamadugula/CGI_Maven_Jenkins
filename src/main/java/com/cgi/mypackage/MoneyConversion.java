28a2fe94053216f7a5ce71a9aeccb3904b7212fb
Pavan Sharma <pavansharma.mca@gmail.com>
2017-10-27 12:51:19 +0530

    Sonar Fixes2

--- src/main/java/com/cgi/mypackage/MoneyConversion.java
+++ src/main/java/com/cgi/mypackage/MoneyConversion.java
@@ -2,7 +2,7 @@ package com.cgi.mypackage;
 
 public class MoneyConversion {
 
-	public double inrToUsd(double inr) {
-		return Math.round(inr / 69);
-	}
+    public double inrToUsd(double inr) {
+        return Math.round(inr / 69);
+    }
 }
--- src/test/java/com/cgi/mypackage/MoneyConversionTest.java
+++ src/test/java/com/cgi/mypackage/MoneyConversionTest.java
@@ -6,10 +6,10 @@ import junit.framework.Assert;
 
 public class MoneyConversionTest {
 
-	@Test
-	public void testInrToUsd() {
-		double inr = 1000000;
-		double usd = Math.round((double) inr / 69);
-		Assert.assertEquals("USD", usd, (new MoneyConversion()).inrToUsd(inr));
-	}
+    @Test
+    public void testInrToUsd() {
+        double inr = 1000000;
+        double usd = Math.round((double) inr / 69);
+        Assert.assertEquals("USD", usd, (new MoneyConversion()).inrToUsd(inr));
+    }
 }
